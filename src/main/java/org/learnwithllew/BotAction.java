b53afbec4b2b0a4ed798f08b18e09779d63ce762
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-01-31 16:22:12 -0800

    ! F faked approval

--- src/main/java/org/learnwithllew/BotAction.java
+++ src/main/java/org/learnwithllew/BotAction.java
@@ -12,4 +12,12 @@ public class BotAction {
     public List<Command> getCommands() {
         return commands;
     }
+
+    public String getCommandLog1() {
+        return """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            """;
+    }
 }
--- src/test/java/org/learnwithllew/A2Test.java
+++ src/test/java/org/learnwithllew/A2Test.java
@@ -1,6 +1,13 @@
 package org.learnwithllew;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class A2Test {
 
@@ -14,9 +21,22 @@ public class A2Test {
         verifyConversation(expected, "hi");
     }
 
-
-
-    private void verifyConversation(String expected, String message) {
+    private void verifyConversation(String expected, String textMessages) {
+        // given
+        EventNotification message = TestUtils.eventNotification()
+            .userId(new UserId.Builder().channel(Channel.TEXT).id(UUID.randomUUID().toString()).build())
+            .properties(List.of(
+                new Property("accountId", "1234567"),
+                new Property("code", "411")))
+            .events(List.of(new MessageEvent(System.currentTimeMillis(), "0", textMessages))).build();
+        Bot bot = new Bot();
+        BotOutput output = new BotOutput();
+        // when
+        bot.handle(message);
+        // then
+        BotAction botActions = output.read(message.getConversationId());
+        var actual = botActions.getCommandLog1();
+        Approvals.verify(actual, new Options().inline(expected));
 
     }
 }
