b12c4d22e31778c8879856f3415df35cac367971
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-02-06 19:44:57 -0800

    . d first run-through

---
+++ src/test/java/org/learnwithllew/A5Test.java
@@ -0,0 +1,221 @@
+package org.learnwithllew;
+
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class A5Test {
+
+    @Test
+    void test1() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            """;
+        verifyConversations(expected, "hi");
+    }
+
+    @Test
+    void test2() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            [Customer]: hi
+            [     Bot]: Hmmm, tell me a little more so I can help you.
+            
+            What would you like to do today?
+            """;
+        verifyConversations(expected, "hi", "hi");
+    }
+
+    @Test
+    void test3() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            [Customer]: hi
+            [     Bot]: Hmmm, tell me a little more so I can help you.
+            
+            What would you like to do today?
+            [Customer]: hi
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            """;
+        verifyConversations(expected, "hi", "hi", "hi");
+    }
+
+    @Test
+    void test4() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            [Customer]: pay bill
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            """;
+        verifyConversations(expected, "hi", "pay bill");
+    }
+
+    @Test
+    void test5() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            [Customer]: pay bill
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: Yes, I'm a customer
+            [     Bot]: transfers to 'self_service'
+            """;
+        verifyConversations(expected, "hi", "pay bill", "Yes, I'm a customer");
+    }
+
+    @Test
+    void test6() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            [Customer]: pay bill
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: no
+            [     Bot]: transfers to 'operator'
+            """;
+        verifyConversations(expected, "hi", "pay bill", "no");
+    }
+
+    @Test
+    void test7() {
+        var expected = """
+            [Customer]: hi
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: What would you like to do today?
+            [Customer]: talk to an operator
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: Yes, I'm a customer
+            [     Bot]: transfers to 'operator'
+            """;
+        verifyConversations(expected, "hi", "talk to an operator", "Yes, I'm a customer");
+    }
+
+    @Test
+    void test8() {
+        var expected = """
+            [Customer]: pay bill
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            """;
+        verifyConversations(expected, "pay bill");
+    }
+
+    @Test
+    void test9() {
+        var expected = """
+            [Customer]: pay bill
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: I like swimming
+            [     Bot]: I'd be happy to help you with this, But first, I need to know: are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            """;
+        verifyConversations(expected, "pay bill", "I like swimming");
+    }
+
+    @Test
+    void test10() {
+        var expected = """
+            [Customer]: pay bill
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: I like coffee
+            [     Bot]: I'd be happy to help you with this, But first, I need to know: are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: I like tea
+            [     Bot]: I'd be happy to help you with this, But first, I need to know: are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: no
+            [     Bot]: transfers to 'operator'
+            """;
+        verifyConversations(expected, "pay bill", "I like coffee", "I like tea", "no");
+    }
+
+    @Test
+    void test11() {
+        var expected = """
+            [Customer]: talk to an operator
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            """;
+        verifyConversations(expected, "talk to an operator");
+    }
+
+    @Test
+    void test12() {
+        var expected = """
+            [Customer]: oh hi there, how are you doing
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            """;
+        verifyConversations(expected, "oh hi there, how are you doing");
+    }
+
+    @Test
+    void test13() {
+        var expected = """
+            [Customer]: walk my dog
+            [     Bot]: Hi there! I'm your virtual assistant.
+            [     Bot]: Let me try to help you.
+            [     Bot]: Are you a customer?
+            [        ]:   1) Yes, I'm a customer
+            [        ]:   2) No, I'm not
+            [Customer]: Yes, I'm a customer
+            [     Bot]: transfers to 'operator'
+            """;
+        verifyConversations(expected, "walk my dog", "Yes, I'm a customer");
+    }
+
+    private void verifyConversations(String expected, String... messages) {
+        BotOutput output = new BotOutput();
+        Bot bot = new Bot(output);
+        var storyBoard = StoryBoard.create(bot, output, messages);
+        Approvals.verify(storyBoard, new Options().inline(expected));
+    }
+}
