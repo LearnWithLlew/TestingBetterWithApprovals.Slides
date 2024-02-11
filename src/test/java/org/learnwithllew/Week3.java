99258f10bd37303a2f412bfa5df63fdc53b9c74c
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-02-11 13:33:55 -0800

    . t start of week 3

--- src/test/java/org/learnwithllew/Week3.java
+++ src/test/java/org/learnwithllew/Week3.java
@@ -1,7 +1,6 @@
 package org.learnwithllew;
 
 import org.approvaltests.Approvals;
-import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Test;
 import org.learnwithllew.week3.StoryBoard;
 
@@ -9,37 +8,24 @@ import org.learnwithllew.week3.StoryBoard;
 public class Week3 {
 
     @Test
-    void testPrettyButtons() {
-        verifyConversations("hi", "pay bill");
+    void botIntroducesItselfAndClarifiesIntent() {
+        verifyConversation("hi");
     }
 
     @Test
-    void testPrettyButtonsInline() {
-        var expected = """
-            [Customer]: hi
-            [     Bot]: Hi there! I'm your virtual assistant.
-            [     Bot]: What would you like to do today?
-            [Customer]: pay bill
-            [     Bot]: Let me try to help you.
-            [     Bot]: Are you a customer?
-            [        ]:   1) Yes, I'm a customer
-            [        ]:   2) No, I'm not
-            """;
-        verifyConversation(expected, "hi", "pay bill");
+    void test2() {
+        verifyConversation("hi", "hi");
     }
 
-    private void verifyConversations(String... messages) {
-        verifyConversation(null, messages);
+    @Test
+    void testPayBill() {
+        verifyConversation("hi", "pay bill", "Yes, I'm a customer");
     }
 
-    private void verifyConversation(String expected, String... messages) {
+    private void verifyConversation(String... messages) {
         BotOutput output = new BotOutput();
         Bot bot = new Bot(output);
         var storyBoard = StoryBoard.create(bot, output, messages);
-        Options options = new Options();
-        if (expected != null) {
-            options = options.inline(expected);
-        }
-        Approvals.verify(storyBoard, options);
+        Approvals.verify(storyBoard);
     }
 }
