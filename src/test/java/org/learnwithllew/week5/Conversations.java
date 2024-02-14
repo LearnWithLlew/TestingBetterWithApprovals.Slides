efd1fa7389ec3e3d8d9d8a5326981e554ba0e9a6
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-02-13 17:13:18 -0800

    . c delete clutter

--- src/test/java/org/learnwithllew/week5/Conversations.java
+++ src/test/java/org/learnwithllew/week5/Conversations.java
@@ -1,26 +1,14 @@
 package org.learnwithllew.week5;
 
-import com.spun.util.ArrayUtils;
-import org.lambda.query.Queryable;
-import org.learnwithllew.CustomerType;
-
-import java.util.Arrays;
-
 public class Conversations {
 
     public String[] messages;
-
-    public CustomerType customerType = CustomerType.UNKNOWN;
-
-    public static Conversations conversation(String... messages) {
-        return new Conversations(messages);
-    }
 
     public Conversations(String... conversations) {
         messages = conversations;
     }
 
     public String printMessages() {
-        return  "[" + String.join(", ", this.messages) + "]";
+        return "[" + String.join(", ", this.messages) + "]";
     }
 }
