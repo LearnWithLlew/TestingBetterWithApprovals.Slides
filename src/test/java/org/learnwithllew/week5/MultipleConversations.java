7477398c88d3efa0e712498b67f2420d76579a89
Lada Kesseler <archie.kesseler@gmail.com>
2025-05-14 13:10:28 -0700

    . t added thenConveration
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- src/test/java/org/learnwithllew/week5/MultipleConversations.java
+++ src/test/java/org/learnwithllew/week5/MultipleConversations.java
@@ -1,11 +1,15 @@
 package org.learnwithllew.week5;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MultipleConversations extends ArrayList<Conversation> {
 
-    public MultipleConversations(Conversation... conversations) {
-        this.addAll(Arrays.asList(conversations));
+    public MultipleConversations(String... messages) {
+        this.thenConversation(messages);
+    }
+
+    public MultipleConversations thenConversation(String... messages) {
+        add(new Conversation(messages));
+        return this;
     }
 }
